ef400a29016291bb52d29ce80066dc74fc2ff267
MinexAutomation <minex.automation@gmail.com>
2019-11-08 12:26:53 -0800

    Added a default configuration-based connection string provider.

---
+++ source/R5T.Tencteria/Code/Configuration/DefaultConfigurationConnectionStringProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+using R5T.Venetia;
+
+
+namespace R5T.Tencteria.Configuration
+{
+    /// <summary>
+    /// A default implementation of a configuration-based connection string provider service.
+    /// The service directly provides the value in the configuration at the <see cref="DefaultConfigurationConnectionStringProvider.ConnectionStringConfigurationPath"/> location.
+    /// </summary>
+    public class DefaultConfigurationConnectionStringProvider : IConnectionStringProvider
+    {
+        public const string ConnectionStringConfigurationPath = "DatabaseConfiguration:ConnectionString";
+
+
+        private IConfiguration Configuration { get; }
+
+
+        public DefaultConfigurationConnectionStringProvider(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public string GetConnectionString()
+        {
+            var connectionString = this.Configuration[DefaultConfigurationConnectionStringProvider.ConnectionStringConfigurationPath];
+            return connectionString;
+        }
+    }
+}
